ec65d53355b4ad7d5c6be864e6ceee59c3063b52
tom.yang <tom.yang@netis.com>
2019-06-26 15:38:10 +0800

    update

--- chdpver.sh
+++ chdpver.sh
@@ -1,1 +1,1 @@
-find -name pom.xml | xargs sed -i 's/<version>4.1.13-SNAPSHOT<\/version>/<version>4.1.13.28<\/version>/'
+find -name pom.xml | xargs sed -i 's/<version>4.1.14-SNAPSHOT<\/version>/<version>4.1.14.1-SNAPSHOT<\/version>/'
--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 s=`git st`
-n="$(echo $s | grep -oP 'DP-\d+')"
+n="$(echo $s | grep -oP 'DP-\d+' | head -1)"
 m="$(echo $s | grep -oP '\w+\.java' | head -1)"
 if [ "$#" -ne 1 ]; then
     git commit -m "$n modifie $m"
